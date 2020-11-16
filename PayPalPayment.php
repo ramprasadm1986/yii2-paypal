b3c628b17cf44ac811dc0683dd3f025eb4946949
Ramprasad <ramprasad@krititech.in>
2020-11-16 17:53:24 +0530

    Inital Commit

--- PayPalPayment.php
+++ PayPalPayment.php
@@ -6,8 +6,9 @@ use PayPalCheckoutSdk\Core\PayPalHttpClient;
 use PayPalCheckoutSdk\Core\SandboxEnvironment;
 use PayPalCheckoutSdk\Core\ProductionEnvironment;
 use PayPalCheckoutSdk\Orders\OrdersCreateRequest;
+use PayPalCheckoutSdk\Orders\OrdersCaptureRequest;
 
-use yii\web\HttpException;
+use PayPalHttp\HttpException;
 use yii\helpers\Url;
 use Yii;
 
@@ -17,22 +18,23 @@ class PayPalPayment
    
     public $live;
     public $intent;
-    public $clientId;
-    public $clientSecret;
+    public $client_id;
+    public $client_secret;
     public $cancel_url;
     public $return_url;
 
     private $environment;
-    private $client; = new PayPalHttpClient($environment);
+    private $client;
 
 
-    public function __construct()
+    private function InitClient()
     {
         
-        if(!$this->live)
-        $this->environment = new SandboxEnvironment($this->clientId, $this->clientSecret);
-        else
-        $this->environment = new ProductionEnvironment($this->clientId, $this->clientSecret);
+        
+       if(!$this->live)
+        $this->environment = new SandboxEnvironment($this->client_id, $this->client_secret);
+       else
+       $this->environment = new ProductionEnvironment($this->client_id, $this->client_secret);
     
         $this->client   = new PayPalHttpClient($this->environment);
        
@@ -40,12 +42,13 @@ class PayPalPayment
     
     
     public function doCheckout($data){
-        
+        $this->InitClient();
         $request = new OrdersCreateRequest();
         $request->prefer('return=representation');
         $request->body = [
                              "intent" => $this->intent,
                              "purchase_units" => [$data],
+                           
                              "application_context" => [
                                   "cancel_url" => $this->cancel_url,
                                   "return_url" => $this->return_url
@@ -54,15 +57,33 @@ class PayPalPayment
 
         try {
             // Call API with your client and get a response for your call
-            $response = $client->execute($request);
+            
+           
+            $response = $this->client->execute($request);
             
             // If call returns body in response, you can get the deserialized version from the result attribute of the response
             return $response;
         }
         catch (HttpException $ex) {
-            return ex;
+            return $ex;
         }
             
+    }
+    
+    public function doCapture($id){
+        $this->InitClient();
+        $request = new OrdersCaptureRequest($id);
+        $request->prefer('return=representation');
+        try {
+            // Call API with your client and get a response for your call
+           $response = $this->client->execute($request);
+            
+            // If call returns body in response, you can get the deserialized version from the result attribute of the response
+           return $response;
+        }catch (HttpException $ex) {
+           return $ex;
+        }
+        
     }
     
     
